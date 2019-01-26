6d0d5c3962e0076608d0b8f94835527492b891e4
Ellery <hello@ellerynz.com>
2019-01-26 21:37:41 +1300

    Add basic offline detection

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Todo</router-link> |
       <router-link to="/about">About</router-link>
+    </div>
+    <div v-show="offline">
+      <h2>You are offline! Not everything will work</h2>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    online: true,
+  }),
+  computed: {
+    offline() {
+      return !this.online;
+    },
+  },
+  methods: {
+    updateConnectionStatus() {
+      this.online = navigator.onLine;
+      // navigator.onLine && flushQueue();
+    },
+  },
+  mounted() {
+    window.addEventListener('online', this.updateConnectionStatus);
+    window.addEventListener('offline', this.updateConnectionStatus);
+  },
+  beforeDestroy() {
+    window.removeEventListener('online', this.updateConnectionStatus);
+    window.removeEventListener('offline', this.updateConnectionStatus);
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
